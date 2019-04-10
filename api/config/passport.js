1f2752c72ab0b449652b2295093a5e78e47cef10
tripheo0412 <tripheo0412@gmail.com>
2019-04-10 23:46:02 +0300

    update auth strategy

--- api/config/passport.js
+++ api/config/passport.js
@@ -15,12 +15,18 @@ passport.use(
     },
     (email, password, cb) => {
       //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
-      return userController.user_find_one({ email, password })
+      return userController
+        .user_find_one(email)
         .then(user => {
-          if (!user) {
-            return cb(null, false, { message: "Incorrect email or password." })
-          }
-          return cb(null, user, { message: "Logged In Successfully" })
+          // Match password
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err
+            if (isMatch) {
+              return cb(null, user)
+            } else {
+              return cb(null, false, { message: "Password incorrect" })
+            }
+          })
         })
         .catch(err => cb(err))
     }
@@ -28,30 +34,33 @@ passport.use(
 )
 
 //Protected route
-passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey   : secret
-},
-(jwtPayload, cb) => {
-
-    //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
-    return userController.user_find_one_id(jwtPayload.id)
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: secret
+    },
+    (jwtPayload, cb) => {
+      //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
+      return userController
+        .user_find_one_id(jwtPayload.id)
         .then(user => {
-            return cb(null, user);
+          return cb(null, user)
         })
         .catch(err => {
-            return cb(err);
-        });
-}
-));
+          return cb(err)
+        })
+    }
+  )
+)
 
 //
 passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
+  done(null, user.id)
+})
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
-  });
+passport.deserializeUser((id, done) => {
+  userController.user_find_one_id(id, (err, user) => {
+    done(err, user)
+  })
+})
