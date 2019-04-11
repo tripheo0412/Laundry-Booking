b0db2964936bd8189e26c8f9e3c38f30231d3277
tripheo0412 <tripheo0412@gmail.com>
2019-04-11 23:57:27 +0300

    add machines model

---
+++ api/models/machine.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const MachineSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  building: {
+      type: String,
+      required: true
+  }
+})
+
+const Machines = mongoose.model("Machines", MachineSchema)
+
+module.exports = Machines
