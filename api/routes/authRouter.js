aa2b82316ab5a687b58e66c19841a61630bbf5b8
Tri Hoang <tripheo0412@gmail.com>
2019-04-19 21:41:59 +0300

    Update authRouter.js

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -40,23 +40,7 @@ router.post("/login", (req, res) => {
 
 //POST Register
 router.post("/register", (req, res) => {
-  const { name, email, password, password2, avatarUrl } = req.body
-  let errors = []
-
-  if (!name || !email || !password || !password2) {
-    errors.push({ msg: "Please enter all fields" })
-  }
-
-  if (password != password2) {
-    errors.push({ msg: "Passwords do not match" })
-  }
-
-  if (password.length < 6) {
-    errors.push({ msg: "Password must be at least 6 characters" })
-  }
-  if (errors.length > 0) {
-    res.send(errors)
-  } else {
+  {
       userController.user_find_one_email(req.body.email)
         .then(user => {
           if (user) {
